1bbd985b5059d47ce3353e226a62f6cdccfd1020
davidnovak003@gmail.com <63885138+DavidNovakFERI@users.noreply.github.com>
2025-01-16 15:27:50 +0100

    rmr projekt

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            // Handle login button click
+        }
+
+        private void OnRegisterClicked(object sender, EventArgs e)
+        {
+            // Handle register button click
+        }
+    }
+}
